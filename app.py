8d0cd26d426d68ff86908e579161acb45c8983bb
Jaye <carsurta@hotmail.co.kr>
2021-01-29 12:45:57 +0900

    GIST를 활용한 최근 게시글 파싱 형태로 수정

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
 import re
+import json
 from urllib import parse
-from datetime import datetime, timedelta
 import requests
 
-recent = datetime.now() - timedelta(minutes=15)
 items = ['문화상품권']
-
 bot_id = os.getenv('BOT_ID')
 chat_id = os.getenv('CHAT_ID')
+gist_url = f'https://api.github.com/gists/{os.getenv("GIST_ID")}'
+recent_link = requests.get(gist_url).json()['files']['recent_link']['content']
 url_base = 'http://www.ppomppu.co.kr/zboard/'
 list_url = 'zboard.php?id=ppomppu&page_num=20&search_type=sub_memo&keyword='
 noti_url = f'https://api.telegram.org/bot{bot_id}/sendMessage'
@@ -16,19 +16,31 @@ notis = []
 for item in items:
     target_url = url_base + list_url + parse.quote(item)
     response = requests.get(target_url).text
-    times =  [datetime.strptime(t, '%y.%m.%d %H:%M:%S') for t in re.findall(r"(?<=<td nowrap class='eng list_vspace' colspan=2  title=\").*?(?=\" >)", response)]
-    if times:
-        is_recent = [t > recent for t in times]
+    links = re.findall(r"(?<=<a href=\")view.php\?id=ppomppu&.*?(?=\")", response)
+    if links:
         try:
-            links = re.findall(r"(?<=<a href=\")view.php\?id=ppomppu&.*?(?=\")", response)
-            rs = is_recent.index(False)
+            rs = links.index(recent_link)
             if rs:
                 notis += reversed([url_base + links[i] for i in range(rs)])
         except:
             notis += reversed([url_base + l for l in links])
     else:
-        notis += ['시간 파싱 오류 발생']
+        notis += [f'{item} 게시글 링크 파싱 오류 발생']
     if notis:
+        if links:
+            gist_headers = {
+                'Accept':'application/vnd.github.v3+json',
+                'Authorization':f'token {os.getenv("GIST_TOKEN")}'
+            }
+            gist_data = {
+                'description':'update recent link',
+                'files':{
+                    'recent_link': {
+                        'content': links[0]
+                    }
+                }
+            }
+            requests.patch(gist_url, headers=gist_headers, data=json.dumps(gist_data))
         for n in notis:
             noti_params = {
                 'chat_id': chat_id,

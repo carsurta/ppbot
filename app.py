db5c5a132a10cb51cd22f77cd81fa45b2b8d48de
carsurta <35985812+carsurta@users.noreply.github.com>
2021-03-27 14:18:51 +0900

    Update app.py
    구매목록에 공유기 추가

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 from urllib import parse
 import requests
 
-items = ['문화상품권']
+items = ['문화상품권', '공유기']
 bot_id = os.getenv('BOT_ID')
 chat_id = os.getenv('CHAT_ID')
 gist_url = f'https://api.github.com/gists/{os.getenv("GIST_ID")}'

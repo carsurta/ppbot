827986495ff011292cf77c5e150bf39470279b7f
carsurta <35985812+carsurta@users.noreply.github.com>
2021-03-28 23:39:32 +0900

    Update app.py
    여러 품목 대응 수정

--- app.py
+++ app.py
@@ -4,22 +4,24 @@ import json
 from urllib import parse
 import requests
 
-items = ['문화상품권']
+items = ['문화상품권', '공유기']
 bot_id = os.getenv('BOT_ID')
 chat_id = os.getenv('CHAT_ID')
 gist_url = f'https://api.github.com/gists/{os.getenv("GIST_ID")}'
-recent_link = requests.get(gist_url).json()['files']['recent_link']['content']
+recents = requests.get(gist_url).json()['files']['recents']['content'].split('\n')
 url_base = 'http://www.ppomppu.co.kr/zboard/'
 list_url = 'zboard.php?id=ppomppu&page_num=20&search_type=sub_memo&keyword='
 noti_url = f'https://api.telegram.org/bot{bot_id}/sendMessage'
 notis = []
-for item in items:
+while len(items) > len(recents):
+    recents.append('')
+for idx, item in enumerate(items):
     target_url = url_base + list_url + parse.quote(item)
     response = requests.get(target_url).text
     links = re.findall(r"(?<=<a href=\")view.php\?id=ppomppu&.*?(?=\")", response)
     if links:
         try:
-            rs = links.index(recent_link)
+            rs = links.index(recents[idx])
             if rs:
                 notis += reversed([url_base + links[i] for i in range(rs)])
         except:
@@ -28,22 +30,23 @@ for item in items:
         notis += [f'{item} 게시글 링크 파싱 오류 발생']
     if notis:
         if links:
-            gist_headers = {
-                'Accept':'application/vnd.github.v3+json',
-                'Authorization':f'token {os.getenv("GIST_TOKEN")}'
-            }
-            gist_data = {
-                'description':'update recent link',
-                'files':{
-                    'recent_link': {
-                        'content': links[0]
-                    }
-                }
-            }
-            requests.patch(gist_url, headers=gist_headers, data=json.dumps(gist_data))
+            recents[idx] = links[0]
         for n in notis:
             noti_params = {
                 'chat_id': chat_id,
                 'text': n
             }
             requests.get(noti_url, noti_params)
+gist_headers = {
+    'Accept':'application/vnd.github.v3+json',
+    'Authorization':f'token {os.getenv("GIST_TOKEN")}'
+}
+gist_data = {
+    'description':'update recent link',
+    'files':{
+        'recents': {
+            'content': '\n'.join(recents)
+        }
+    }
+}
+requests.patch(gist_url, headers=gist_headers, data=json.dumps(gist_data))

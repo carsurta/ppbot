6785adffd38d35ef096cb00cc29072fa79089613
carsurta <35985812+carsurta@users.noreply.github.com>
2021-04-21 22:49:37 +0900

    상품 추가

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 from urllib import parse
 import requests
 
-items = ['문화상품권', '공유기']
+items = ['문화상품권', '공유기', '필립스 칫솔모', '제로콜라', '코크제로', '질레트 면도날']
 bot_id = os.getenv('BOT_ID')
 chat_id = os.getenv('CHAT_ID')
 gist_url = f'https://api.github.com/gists/{os.getenv("GIST_ID")}'
